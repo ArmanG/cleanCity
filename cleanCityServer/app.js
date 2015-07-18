462d1870c0fa55e98f3aaccb6d87e1284b1ba9e9
Vitaliy Krasylovets <Vitaliy@Vitaliys-MacBook-Pro.local>
2015-07-18 15:20:17 -0400

    Added get all beacons and add beacon to server

--- cleanCityServer/app.js
+++ cleanCityServer/app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var beacons = require('./routes/beacons');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/beacons', beacons);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
