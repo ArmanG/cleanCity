dd5fef350aaf9354a2197ab3b1754db8fdab18a2
Vitaliy Krasylovets <Vitaliy@Vitaliys-MacBook-Pro.local>
2015-07-18 22:46:51 -0400

    Create a json parser for Beacon

--- cleanCityApp/app/src/main/java/com/battlehack/cleancity/cleancity/Models/Beacon.java
+++ cleanCityApp/app/src/main/java/com/battlehack/cleancity/cleancity/Models/Beacon.java
@@ -1,29 +1,76 @@
 package com.battlehack.cleancity.cleancity.Models;
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
 /**
  * Created by Vitaliy on 15-07-18.
  */
 public class Beacon {
 
-    private int id;
+    private String id;
     private String name;
     private double latitude, longitude;
     private boolean full;
     private double distance;
 
+    public Beacon () {
+        this.name = "";
+        this.latitude = 0;
+        this.longitude = 0;
+        this.id = "";
+        this.distance = -1.0;
+    }
+
     public Beacon (String name, double latitude, double longitude) {
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
-        this.id = 0;
+        this.id = "";
         this.distance = -1.0;
     }
 
-    public int getId() {
+    public static Beacon getBeaconFromJson(JSONObject jsonObject) throws JSONException {
+        Beacon beacon = new Beacon();
+
+        // gets data from a json object
+
+        Iterator<String> iter = jsonObject.keys();
+        while (iter.hasNext()) {
+            String key = iter.next();
+            Log.d("BEACON", key);
+
+            if (key.equals("_id")) {
+                beacon.setId(jsonObject.getString(key));
+            } else if (key.equals("name")) {
+                beacon.setName(jsonObject.getString(key));
+            } else if (key.equals("loc")) {
+                JSONArray loc = jsonObject.getJSONArray("loc");
+
+                if (loc != null) {
+                    beacon.setLatitude(loc.getDouble(0));
+                    beacon.setLongitude(loc.getDouble(1));
+                }
+            } else if (key.equals("full")) {
+                beacon.setFull(jsonObject.getBoolean(key));
+            } else if (key.equals("dist")) {
+                beacon.setDistance(jsonObject.getDouble(key));
+            }
+        }
+
+        return beacon;
+    }
+
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
